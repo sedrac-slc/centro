aed7a41c16ab185cf4f404bb34ac3c69b95f70c8
apoio-forLEARN <apoio@forlearn.ao>
2023-07-25 16:50:06 +0100

    replace migrate two

--- database/migrations/2023_06_18_187604_create_curso_disciplina_table.php
+++ database/migrations/2023_06_18_187604_create_curso_disciplina_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('curso_disciplina', function (Blueprint $table) {
+        Schema::create('', function (Blueprint $table) {
             $table->id();
             $table->foreignId('curso_id')->constrained('cursos')->cascadeOnDelete();
             $table->foreignId('disciplina_id')->constrained('disciplinas')->cascadeOnDelete();
